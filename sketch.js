f485a93c0c8ff4b5aa9eecd68c63e5ee6078d377
RaghavAgarwal08 <94966635+RaghavAgarwal08@users.noreply.github.com>
2021-12-22 12:26:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var student;
+
+function setup() {
+  createCanvas(400,400);
+  student = new Student("john", 12, 6);
+  student.show()
+
+}
+
+function draw() 
+{
+  background(30);
+  
+  
+}
---
+++ student.js
@@ -0,0 +1,17 @@
+
+class Student
+{
+   constructor(name, age, standard){
+      this.n = name;
+      this.a = age;
+      this.s = standard;
+   }
+   show(){
+       console.log(this.n);
+       console.log(this.a);
+       console.log(this.s);
+   }
+}
+
+
+
